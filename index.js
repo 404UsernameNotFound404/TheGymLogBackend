99f2dfdcf82ee15fe1277608f9ccb0b39f2d2c55
Henry M <2001henrymorris@gmail.com>
2021-05-07 19:26:41 -0400

    real

--- index.js
+++ index.js
@@ -11,30 +11,26 @@ app.use(bodyParser.urlencoded({ extended: true }), cors());
 
 console.log(process.env.DB_URL);
 console.log(process.env.PORT);
-// MongoClient.connect(process.env.DB_URL, { useNewUrlParser: true }, (err, database) => {
-//     if (err) {
-//         console.log(
-//             "Error occurred while connecting to MongoDB Atlas...\n",
-//             err
-//         );
-//     } else {
-//         console.log("connected");
-//         require("./src/routes")(app, database);
-//         // let port;
-//         // portfinder.getPort((err, openPort) => {
-//         //     if (err) {
-//         //         console.log(err);
-//         //     }
-//         //     app.listen(8000, () => {
-//         //         console.log("Listening on port " + 8000);
-//         //     });
-//         // });
-//         app.listen(process.env.PORT, () => {
-//             console.log("listening on port " + process.env.PORT);
-//         });
-//     }
-// });
-
-app.listen(process.env.PORT, () => {
-    console.log("listening on port " + process.env.PORT);
+MongoClient.connect(process.env.DB_URL, { useNewUrlParser: true }, (err, database) => {
+    if (err) {
+        console.log(
+            "Error occurred while connecting to MongoDB Atlas...\n",
+            err
+        );
+    } else {
+        console.log("connected");
+        require("./src/routes")(app, database);
+        // let port;
+        // portfinder.getPort((err, openPort) => {
+        //     if (err) {
+        //         console.log(err);
+        //     }
+        //     app.listen(8000, () => {
+        //         console.log("Listening on port " + 8000);
+        //     });
+        // });
+        app.listen(process.env.PORT, () => {
+            console.log("listening on port " + process.env.PORT);
+        });
+    }
 });
